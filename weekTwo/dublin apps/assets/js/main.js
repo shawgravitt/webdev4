0725a4fac23d5d004c2e007ed3b7e24f5d0b4f88
Shaw Gravitt <smgravitt@gmail.com>
2014-07-23 15:49:59 -0400

    the latest versions so far, porkypig is done, dublin apps still needs some work

---
+++ weekTwo/dublin apps/assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	
+	$( "#left-arrow, #right-arrow" ).click(function(event) {
+		$(".left-col, .ipadImg").toggleClass('hidden');
+		$(".sheen").toggleClass('none');
+	});
+
+	
+
+
+
+});
